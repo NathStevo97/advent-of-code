4540d1b26dd7cb294c62919bba04596f522bd207
NathStevo97 <natstephenson15@gmail.com>
2024-12-01 22:30:42 +0000

    feat: 2024 day 01

---
+++ 2023/day_01/main.py
@@ -0,0 +1,4 @@
+with open('./input.txt') as file:
+    params = file.read().split('\n') # read input file and split into groups based on new lines
+
+print(params)
---
+++ 2024/day_01/main.py
@@ -0,0 +1,27 @@
+# Open file and split rows into columns
+with open('./input.txt') as file:
+    columns = list(zip(*(map(int, row.split()) for row in file))) # read input file and split into groups based on new lines
+
+# Sort each column into numerically ascending order
+sortedColumn1 = sorted(columns[0], key=int, reverse=False)
+sortedColumn2 = sorted(columns[1], key=int, reverse=False)
+
+diffs = []
+
+# calculate the absolute difference between each row's values
+for i in range(0, len(columns[0])):
+    rowDiff = abs(sortedColumn2[i] - sortedColumn1[i])
+    diffs.append(rowDiff)
+
+totalDiffs = sum(diffs)
+print(f"Sum of differences across each row: {totalDiffs}")
+
+# Part 2
+
+similarityScore = 0
+
+for i in sortedColumn1:
+    column2Count = sortedColumn2.count(i)
+    similarityScore = similarityScore + i * column2Count
+
+print(f"Final Similarity Score {similarityScore}")
